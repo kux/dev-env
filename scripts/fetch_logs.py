fffab3a410512e688d4e79ea5cde931263f02f33
Alex Cucu <alexandruioan.cucu@gmail.com>
2013-03-26 13:50:32 +0200

    Improvements to the fetch_logs and merge_logs scripts.

--- scripts/fetch_logs.py
+++ scripts/fetch_logs.py
@@ -7,30 +7,26 @@ from fabric.operations import get
 env.hosts = ['root@ec2-23-23-66-181.compute-1.amazonaws.com', 'root@ec2-54-234-73-236.compute-1.amazonaws.com']
 
 
-def _get_logs_dir(deployment):
-    return'./logs/%s' % deployment
-    
-
-def _fetch_logs(log_type, vhost, days, dst):
-    src = '/var/log/httpd/%s%s.log' % (vhost, log_type)
+def _fetch_logs(log_type, remote_logs_dir, vhost, days, dst):
+    src = os.path.join(remote_logs_dir, '%s%s.log' % (vhost, log_type))
     filename = os.path.basename(src)
     get(src, '%s/%s.%s' % (dst, filename, env.host))
     for day in xrange(days):
-        src = '/var/log/httpd/%s%s.log.%d' % (vhost, log_type, day + 1)
+        src = os.path.join(remote_logs_dir, '%s%s.log.%d' % (vhost, log_type, day + 1))
         filename = os.path.basename(src)
-        get(src, '%s/%s.%s' % (dst, filename, env.host))
+        get(src, os.path.join(dst, '%s.%s' % (filename, env.host)))
 
 
-def fetch_logs(deployment, vhost='', days=0, access=True, error=False):
+def fetch_logs(dst, remote_logs_dir='/var/log/httpd', vhost='', days=0, access=True, error=False):
     if vhost:
         vhost = vhost + '_'
     days = int(days)
-    dst = _get_logs_dir(deployment)
+
     if os.path.exists(dst) and not os.path.isdir(dst):
         raise ValueError('Destination must be a folder')
     if not os.path.exists(dst):
         os.mkdir(dst)
     if access:
-        _fetch_logs('access', vhost, days, dst)
+        _fetch_logs('access', remote_logs_dir, vhost, days, dst)
     if error:
-        _fetch_logs('error', vhost, days, dst)
+        _fetch_logs('error', remote_logs_dir, vhost, days, dst)
--- scripts/merge_logs.py
+++ scripts/merge_logs.py
@@ -2,92 +2,107 @@ import copy
 import datetime
 import os
 import re
+import sys
 import time
 
-from optparse import OptionParser
+
+APACHE_TIMESTAMP_REGEX = re.compile(r'\[(\d\d)/(\w\w\w)/(\d\d\d\d):(\d\d):(\d\d):(\d\d) -\d*\]')
+
+MONTHS = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6,
+          'Jul': 7, 'Aug': 8, 'Sep': 9, 'Nov': 10, 'Oct': 11, 'Dec': 12}
 
 
-def _nway_merge(dst, logs, destination_log, concurrent_post_delta=5):
-    months = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6,
-              'Jul': 7, 'Aug': 8, 'Sep': 9, 'Nov': 10, 'Oct': 11, 'Dec': 12}
-    try:
-        fds = [open(log) for log in logs]
-    except IOError:
-        # if one of the files failes to open fds will point at the last succesfully opened file
-        # the previous opened files are instantly closed by garbage collection
-        try:
-            fds.close()
-        except NameError:
-            # no file got opened
-            pass
+def _get_timestamp_from_log_line(line):
+    match = re.search(APACHE_TIMESTAMP_REGEX, line)
+    if match:
+        day, month, year, hour, mn, sec = match.groups()
+        return datetime.datetime(int(year), MONTHS[month], int(day), int(hour), int(mn), int(sec))
     else:
-        fds_copy = copy.copy(fds)
-        destination_fd = open(destination_log, 'w')
-        post_log_fd = open(destination_log + '.post', 'w')
-        prev_post = 0
-        prev_post_line = ''
-        prev_is_written = False
-        try:
-            current_lines = [fd.readline() for fd in fds]
-            while fds:
-                dates = []
-                for index, line in enumerate(current_lines[:]):
-                    if line == '':
-                        del fds[index]
-                        del current_lines[index]
-                    else:
-                        match = re.search(r'\[(\d\d)/(\w\w\w)/(\d\d\d\d):(\d\d):(\d\d):(\d\d) -\d*\]', line)
-                        if match:
-                            day, month, year, hour, mn, sec = match.groups()
-                            ordered_tup = (int(year), months[month], int(day), int(hour), int(mn), int(sec))
-                            dates.append(ordered_tup)
-                if dates:
-                    min_date = min(dates)
-                    min_index = dates.index(min_date)
-                    to_write = current_lines[min_index]
-                    destination_fd.write(to_write)
-                    is_post = re.search(r'request="POST', to_write) is not None
-                    if is_post:
-                        year, month, day, hour, mn, sec = min_date
-                        post_time = time.mktime(datetime.datetime(year, month, day, hour, mn, sec).timetuple())
-                        if post_time < prev_post + concurrent_post_delta:
-                            if not prev_is_written:
-                                post_log_fd.write('\n----------------new burst----------------\n')
-                                post_log_fd.write(prev_post_line)
-                            post_log_fd.write(to_write)
-                            prev_is_written = True
-                        else:
-                            prev_is_written = False
-                        prev_post = post_time
-                        prev_post_line = to_write
-
-                    current_lines[min_index] = fds[min_index].readline()
-        finally:
-            for fd in fds_copy:
-                fd.close()
-            destination_fd.close()
-            post_log_fd.close()
+        raise ValueError("Line %s doesn't contian a timestamp in expected format" % line)
 
 
-def merge_logs(logs_dir, hosts, vhost='', days=0):
-    if vhost:
-        vhost = vhost + '_'
-    days = int(days)
+def _skip_empty_logs(current_lines, fds):
+    indexes_to_remove = set()
+    for index, line in enumerate(current_lines[:]):
+        if line == '':
+            indexes_to_remove.add(index)
+    current_lines[:] = [ln for (i, ln) in enumerate(current_lines)
+                        if i not in indexes_to_remove]
+    fds[:] = [fd for (i, fd) in enumerate(fds) if i not in indexes_to_remove]
 
-    def do_merge(day=None):
-        base_log_path = os.path.join(logs_dir, '%saccess.log' % vhost)
-        if day is not None:
-            base_log_path = '%s.%d' % (base_log_path, day)
-        logs = ['%s.%s' % (base_log_path, host) for host in hosts]
-        destination_log = '%s/%saccess.log' % (logs_dir, vhost)
+
+def _nway_merge(logs, merged_log, burst_regex, burst_delta=5):
+    fds = [open(log) for log in logs]
+    fds_copy = copy.copy(fds)
+    merged_fd = open(merged_log, 'w')
+    post_log_fd = open(merged_log + '.post', 'w')
+    prev_post = 0
+    prev_post_line = ''
+    prev_is_written = False
+    try:
+        current_lines = [fd.readline() for fd in fds]
+        _skip_empty_logs(current_lines, fds)
+        timestamps = [_get_timestamp_from_log_line(ln) for ln in current_lines]
+        while fds:
+            assert len(timestamps) == len(current_lines) == len(fds)
+            min_timestamp = min(timestamps)
+            min_index = timestamps.index(min_timestamp)
+            line_to_write = current_lines[min_index]
+            merged_fd.write(line_to_write)
+            is_post = re.search(burst_regex, line_to_write) is not None
+            if is_post:
+                post_time = time.mktime(min_timestamp.timetuple())
+                if post_time < prev_post + burst_delta:
+                    if not prev_is_written:
+                        post_log_fd.write('\n--- new burst ---\n')
+                        post_log_fd.write(prev_post_line)
+                    post_log_fd.write(line_to_write)
+                    prev_is_written = True
+                else:
+                    prev_is_written = False
+                prev_post = post_time
+                prev_post_line = line_to_write
+            new_line = fds[min_index].readline()
+            if new_line != '':
+                current_lines[min_index] = new_line
+                timestamps[min_index] = _get_timestamp_from_log_line(new_line)
+            else:
+                del current_lines[min_index]
+                del fds[min_index]
+                del timestamps[min_index]
+    finally:
+        for fd in fds_copy:
+            fd.close()
+        merged_fd.close()
+        post_log_fd.close()
+
+
+
+def merge_logs(logs_dir, burst_regex, burst_delta):
+    log_files = os.listdir(logs_dir)
+    logs_by_day = {}
+    log_regex = r'(?:(\w*)_)?(access|error)\.log(?:\.(\d+))?\.(\w+)'
+    for log_file in log_files:
+        match = re.search(log_regex, log_file)
+        if match:
+            vhost, log_type, day, host = match.groups()
+            if host == 'post':
+                continue
+            logs_by_day.setdefault((vhost, log_type, day), []).append(os.path.join(logs_dir, log_file))
+    for details, logs in logs_by_day.iteritems():
+        vhost, log_type, day = details
+        aggregated_log = '%s.log' % log_type
+        if vhost:
+            aggregated_log = '%s_%s' % (vhost, aggregated_log)
         if day:
-            destination_log = '%s.%d' % (destination_log, day)
-        _nway_merge(logs_dir, logs, destination_log)
-        
-    do_merge(None)
-    for day in xrange(1, days + 1):
-        do_merge(day)
+            aggregated_log = '%s.%s' % (aggregated_log, day)
+        aggregated_log = os.path.join(logs_dir, aggregated_log)
+        _nway_merge(logs, aggregated_log, burst_regex, burst_delta)
 
 
 if __name__ == '__main__':
-    parser = OptionParser()
+    if len(sys.argv) != 4:
+        print 'Usage: python merge_logs.py logs_dir burst_regex burst_delta'
+    _, logs_dir, burst_regex, burst_delta = sys.argv
+    burst_regex = re.compile(burst_regex)
+    merge_logs(logs_dir, burst_regex, int(burst_delta))
