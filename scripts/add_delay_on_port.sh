f1e338493ce4d0b7ad54e0f2a55e971d4bb25f6e
Alex Cucu <alexandruioan.cucu@gmail.com>
2013-04-22 20:43:11 +0300

    s/device/interface

--- scripts/add_delay_on_port.sh
+++ scripts/add_delay_on_port.sh
@@ -1,8 +1,8 @@
-device=$1
+interface=$1
 delay=$2
 port=$3
-tc qdisc add dev $device handle 1: root htb
-tc class add dev $device parent 1: classid 1:1 htb rate 100Mbps
-tc qdisc add dev $device parent 1:1 handle 10: netem delay $delay
-tc filter add dev $device protocol ip parent 1: prio 1 u32 match ip dport $port 0xffff flowid 1:1
+tc qdisc add dev $interface handle 1: root htb
+tc class add dev $interface parent 1: classid 1:1 htb rate 100Mbps
+tc qdisc add dev $interface parent 1:1 handle 10: netem delay $delay
+tc filter add dev $interface protocol ip parent 1: prio 1 u32 match ip dport $port 0xffff flowid 1:1
 tc filter add dev $device protocol ip parent 1: prio 1 u32 match ip sport $port 0xffff flowid 1:1
