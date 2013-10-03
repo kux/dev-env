ddcf6039b30708957cd16492eb1234607cf054d7
Cucu <ioancucu@u4437e6868aba52455508.ant.amazon.com>
2013-10-03 13:02:26 +0100

    Add emacs goodies in the list of things to install (required by solarized colour theme)

--- emacs_cfg/install_deps.sh
+++ emacs_cfg/install_deps.sh
@@ -1,3 +1,3 @@
-apt-get install python-pip pyflakes pylint pep8 pychecker yasnippet python-ropemacs
+apt-get install python-pip pyflakes pylint pep8 pychecker yasnippet python-ropemacs emacs-goodies-el
 pip install ipython
 pip install ipdb
