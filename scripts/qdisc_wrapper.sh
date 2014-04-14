af164f826022274f563865428117721e7dc66102
Ioan Cucu <ioancucu@amazon.com>
2014-04-14 08:31:51 +0000

    Reset previous qdiscs before applying the new rules

--- scripts/qdisc_wrapper.sh
+++ scripts/qdisc_wrapper.sh
@@ -3,9 +3,12 @@ delay=$2
 pattern=$3
 mask=$4
 offset=$5
+
+tc qdisc del dev $interface root 
+
 tc qdisc add dev $interface handle 1: root htb
 tc class add dev $interface parent 1: classid 1:1 htb rate 100Mbps
 tc qdisc add dev $interface parent 1:1 handle 10: netem delay $delay
-tc filter add dev lo protocol ip parent 1: prio 1 u32 match u32 $pattern $mask at $offset flowid 1:1
+tc filter add dev $interface protocol ip parent 1: prio 1 u32 match u32 $pattern $mask at $offset flowid 1:1
 
 # tc qdisc del dev $interface root 
